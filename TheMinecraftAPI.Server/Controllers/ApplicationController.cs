4f29fa36a6e168e7c85068c847483b1ffa25dde0
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-03-19 18:18:45 -0400

    Add ApplicationController to TheMinecraftAPI
    This controller will be used for handling incoming requests to "/api". It utilizes Microsoft.AspNetCore.Mvc and currently includes a GET method that returns application data.

---
+++ TheMinecraftAPI.Server/Controllers/ApplicationController.cs
@@ -0,0 +1,18 @@
+﻿// LFInteractive LLC. 2021-2024
+
+using Microsoft.AspNetCore.Mvc;
+using TheMinecraftAPI.Server.Data;
+
+namespace TheMinecraftAPI.Server.Controllers;
+
+[Produces("application/json")]
+[Route("/api")]
+[ApiController]
+public class ApplicationController : ControllerBase
+{
+    [HttpGet()]
+    public IActionResult Get()
+    {
+        return Ok(ApplicationData.GenerateApplicationData());
+    }
+}
