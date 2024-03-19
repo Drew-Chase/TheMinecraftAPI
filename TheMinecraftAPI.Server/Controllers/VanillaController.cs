5ab776cd5a77abfe2f72c8265d94ca8ec785be70
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-03-19 18:18:38 -0400

    Add VanillaController to handle server status requests
    A new controller named VanillaController is added to handle GET requests for server status. This controller takes a URL and port (default 25565) as parameters and gets the server status from the MinecraftServers class. The response is then formatted as a JSON content result.

---
+++ TheMinecraftAPI.Server/Controllers/VanillaController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using TheMinecraftAPI.Vanilla;
+
+namespace TheMinecraftAPI.Server.Controllers;
+
+[Produces("application/json")]
+[Route("/api/minecraft/")]
+[ApiController]
+public class VanillaController : ControllerBase
+{
+    [HttpGet("server")]
+    public async Task<IActionResult> GetServerStatusAsync([FromQuery] string url, [FromQuery] int port = 25565)
+    {
+        MinecraftServers server = new MinecraftServers(url, port);
+        var response = await server.GetServerStatusAsync();
+        return new ContentResult
+        {
+            Content = response.ToString(),
+            ContentType = "application/json",
+            StatusCode = 200
+        };
+    }
+}
