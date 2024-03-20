2ed70c0b5628b11412a01a63f37c953ca85ffee9
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-03-20 11:43:26 -0400

    Update VanillaController with error handling and caching
    Adjusted the Route attribute, added try-catch block for error handling in GetServerStatusAsync method, and included ResponseCache to improve response times. Also, a new method GetVersionsAsync was added to get Minecraft versions, which are cached for an hour.

--- TheMinecraftAPI.Server/Controllers/VanillaController.cs
+++ TheMinecraftAPI.Server/Controllers/VanillaController.cs
@@ -1,24 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 using TheMinecraftAPI.Vanilla;
 
 namespace TheMinecraftAPI.Server.Controllers;
 
 [Produces("application/json")]
-[Route("/api/minecraft/")]
+[Route("/minecraft/")]
 [ApiController]
 public class VanillaController : ControllerBase
 {
-    [HttpGet("server")]
+    [HttpGet("server"), ResponseCache(Duration = 60)] // Cache for 1 minute
     public async Task<IActionResult> GetServerStatusAsync([FromQuery] string url, [FromQuery] int port = 25565)
     {
-        MinecraftServers server = new MinecraftServers(url, port);
-        var response = await server.GetServerStatusAsync();
-        return new ContentResult
+        try
         {
-            Content = response.ToString(),
-            ContentType = "application/json",
-            StatusCode = 200
-        };
+            MinecraftServers server = new MinecraftServers(url, port);
+            var response = await server.GetServerStatusAsync();
+            return new ContentResult
+            {
+                Content = JsonConvert.SerializeObject(response),
+                ContentType = "application/json",
+                StatusCode = 200
+            };
+        }
+        catch (Exception e)
+        {
+            return BadRequest(new
+            {
+                ip = url,
+                port = port,
+                error = e.Message
+            });
+        }
+    }
+
+    [HttpGet("versions"), ResponseCache(Duration = 3600)] // Cache for 1 hour
+    public async Task<IActionResult> GetVersionsAsync([FromQuery(Name = "major_version")] Version? majorVersion = null, [FromQuery] bool snapshots = false)
+    {
+        var response = await MinecraftVersions.GetVersions(majorVersion, snapshots);
+        return Ok(response);
     }
 }
