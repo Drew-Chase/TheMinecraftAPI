726b7924476dfd6a6a06c51db55dece3934939bf
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-03-20 20:41:04 -0400

    Add ErrorController to handle server errors
    A new ErrorController has been added to the application. This controller logs errors along with relevant details and returns a custom ContentResult, enabling better error tracking and handling.

---
+++ TheMinecraftAPI.Server/Controllers/ErrorController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace TheMinecraftAPI.Server.Controllers;
+
+[Route("error")]
+[ApiController]
+public class ErrorController : ControllerBase
+{
+    [HttpGet("{code:int}")]
+    public IActionResult Index([FromRoute]int code, [FromQuery] string url)
+    {
+        Log.Error("Error {code} for {url}", code, url);
+        return new ContentResult()
+        {
+            StatusCode = code,
+            ContentType = "application/json",
+            Content = $"{{\"error\":{code},\"url\":\"{url}\"}}"
+        };
+    }
+}
