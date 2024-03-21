9e8b272fd4589a4e75873d03763dfd1d618188ea
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-03-21 18:37:29 -0400

    Add new Author struct in TheMinecraftAPI
    A new struct type for an Author has been added in TheMinecraftAPI, under Platforms/Structs. This structure includes properties for the author's Id, Name, and URL, along with their respective JsonProperty annotations.

---
+++ TheMinecraftAPI.Platforms/Structs/Author.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+
+namespace TheMinecraftAPI.Platforms.Structs;
+
+public struct Author
+{
+    /// <summary>
+    /// Gets or sets the Id of the Author.
+    /// </summary>
+    /// <remarks>
+    /// The Id represents the unique identifier of the author in the system.
+    /// </remarks>
+    [JsonProperty("id")]
+    public string Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the name of the author.
+    /// </summary>
+    /// <remarks>
+    /// This property represents the name of the author.
+    /// </remarks>
+    [JsonProperty("name")]
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Represents the URL of an author.
+    /// </summary>
+    [JsonProperty("url")]
+    public string Url { get; set; }
+}
