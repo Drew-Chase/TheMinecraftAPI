73ef42055a20c1fe648f9f96348ddce6c5d206c5
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-03-20 20:41:33 -0400

    Add PlatformLink struct in TheMinecraftAPI
    This commit introduces a new structure, PlatformLink, to TheMinecraftAPI. This structure is used to hold properties of a platform link, including its name and URL.

---
+++ TheMinecraftAPI.Platforms/structs/PlatformLink.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace TheMinecraftAPI.Platforms.structs;
+
+public struct PlatformLink
+{
+    /// <summary>
+    /// Gets or sets the name of the platform link.
+    /// </summary>
+    [JsonProperty("name")]
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Represents a URL property for a platform link.
+    /// </summary>
+    [JsonProperty("url")]
+    public string Url { get; set; }
+}
