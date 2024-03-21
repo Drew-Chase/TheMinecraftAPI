a1be96660568f5493a6af215fbf64b73ce01c916
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-03-21 18:36:54 -0400

    Add PlatformSearchResults struct
    A new struct named PlatformSearchResults was added in the Platforms.Structs part of the project. This struct contains properties related to the results of a platform search, including information such as Results, TotalResults, Limit, Offset, Query, and a method to check if the Results are empty.

---
+++ TheMinecraftAPI.Platforms/Structs/PlatformSearchResults.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TheMinecraftAPI.Platforms.Structs;
+
+public struct PlatformSearchResults
+{
+    public PlatformModel[] Results { get; set; }
+    public int TotalResults { get; set; }
+    public int Limit { get; set; }
+    public int Offset { get; set; }
+    public string Query { get; set; }
+
+    public static PlatformSearchResults Empty => new()
+    {
+        Results = Array.Empty<PlatformModel>(),
+        TotalResults = 0,
+        Limit = 0,
+        Offset = 0,
+        Query = string.Empty
+    };
+
+    [Newtonsoft.Json.JsonIgnore]
+    [JsonIgnore]
+    public bool IsEmpty => Results.Length == 0;
+}
