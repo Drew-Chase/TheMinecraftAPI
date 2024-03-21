1514277a08801955aa019f7523f514d5e3da568d
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-03-20 20:41:19 -0400

    Add PlatformSource struct to Platforms
    A new struct, PlatformSource, has been added to the Platforms in TheMinecraftAPI. This struct includes properties for the ID and Name of the platform source, serialized with JsonProperty attributes.

---
+++ TheMinecraftAPI.Platforms/structs/PlatformSource.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace TheMinecraftAPI.Platforms.structs;
+
+public struct PlatformSource
+{
+    /// <summary>
+    /// Gets or sets the Id of the platform source.
+    /// </summary>
+    [JsonProperty("id")]
+    public string Id { get; set; }
+
+    /// <summary>
+    /// Represents the Name property of the PlatformSource struct.
+    /// </summary>
+    [JsonProperty("name")]
+    public string Name { get; set; }
+}
