ee409f668057f9d0930364fd1154e374af8b149c
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-03-21 18:37:36 -0400

    Add UniversalClient to unify API calls
    Implemented a new class, UniversalClient, within the Minecraft API. This class is designed to manage and streamline the process of making API requests. It combines multiple client calls, aggregates results, and handles the disposal of client instances.

---
+++ TheMinecraftAPI.Platforms/Clients/UniversalClient.cs
@@ -0,0 +1,91 @@
+﻿using TheMinecraftAPI.Platforms.Structs;
+
+namespace TheMinecraftAPI.Platforms.Clients;
+
+public class UniversalClient : IDisposable, IPlatformClient
+{
+    private readonly IPlatformClient[] _clients =
+    {
+        new ModrinthClient(),
+        new CurseForgeClient()
+    };
+
+    public async Task<PlatformSearchResults> SearchProjects(string query, string projectType, string loader, int limit, int offset)
+    {
+        List<PlatformModel> projects = new();
+        int totalResults = 0;
+        int totalLimit = 0;
+        int totalOffset = 0;
+        var tasks = new Task<PlatformSearchResults>[_clients.Length];
+
+        for (int i = 0; i < _clients.Length; i++)
+        {
+            var client = _clients[i];
+            tasks[i] = client.SearchProjects(query, projectType, loader, limit, offset);
+        }
+
+        await Task.WhenAll(tasks);
+
+        foreach (var task in tasks)
+        {
+            PlatformSearchResults item = task.Result;
+            if (item.IsEmpty) continue;
+            projects.AddRange(item.Results);
+            totalResults += item.TotalResults;
+            totalLimit += item.Limit;
+            totalOffset += item.Offset;
+        }
+
+        return new PlatformSearchResults()
+        {
+            Results = projects.OrderBy(i => i.Downloads).ToArray(),
+            TotalResults = totalResults,
+            Limit = totalLimit,
+            Offset = totalOffset,
+        };
+    }
+
+
+    public async Task<PlatformModel> GetProject(string id, string type)
+    {
+        foreach (var client in _clients)
+        {
+            var project = await client.GetProject(id, type);
+            if (!project.IsEmpty) return project;
+        }
+
+        return PlatformModel.Empty;
+    }
+
+    public async Task<string> GetProjectIcon(string id, string type)
+    {
+        foreach (var client in _clients)
+        {
+            var icon = await client.GetProjectIcon(id, type);
+            if (!string.IsNullOrWhiteSpace(icon)) return icon;
+        }
+
+        return string.Empty;
+    }
+
+    public async Task<string> GetAuthorProfileImage(string username)
+    {
+        foreach (var client in _clients)
+        {
+            var image = await client.GetAuthorProfileImage(username);
+            if (!string.IsNullOrWhiteSpace(image)) return image;
+        }
+
+        return string.Empty;
+    }
+
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+        foreach (var client in _clients)
+        {
+            if (client is IDisposable disposable) disposable.Dispose();
+        }
+    }
+}
