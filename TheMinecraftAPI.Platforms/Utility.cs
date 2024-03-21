53c75b64baf643baf3f75432f3328830df97be1c
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-03-21 18:36:26 -0400

    Add image URL to Base64 converter in Utility class
    A new static async method named "ConvertUrlImageToBase64" is introduced in the Utility class, under the 'TheMinecraftAPI.Platforms' namespace. It uses HttpClient to get the image from the input URL and then converts it to Base64 string for storage or further use.

---
+++ TheMinecraftAPI.Platforms/Utility.cs
@@ -0,0 +1,13 @@
+﻿namespace TheMinecraftAPI.Platforms;
+
+public class Utility
+{
+    public static async Task<string> ConvertUrlImageToBase64(string url)
+    {
+        using HttpClient client = new();
+        await using Stream stream = await client.GetStreamAsync(url);
+        using MemoryStream memoryStream = new();
+        await stream.CopyToAsync(memoryStream);
+        return $"data:image/png;base64,{Convert.ToBase64String(memoryStream.ToArray())}";
+    }
+}
