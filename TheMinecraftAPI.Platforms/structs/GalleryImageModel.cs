68f8c060f0f080140083af217002cd1e2c8de27c
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-03-20 20:41:39 -0400

    Add GalleryImageModel struct
    A new struct, GalleryImageModel, has been added to the Platforms directory. This struct is designed to store important details related to gallery images like name, description, the URL of the image, and the creation date.

---
+++ TheMinecraftAPI.Platforms/structs/GalleryImageModel.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+
+namespace TheMinecraftAPI.Platforms.structs;
+
+public struct GalleryImageModel
+{
+    /// <summary>
+    /// Represents the name of a gallery image.
+    /// </summary>
+    /// <remarks>
+    /// This property is used in the <see cref="GalleryImageModel"/> struct to store the name of the image.
+    /// </remarks>
+    [JsonProperty("name")]
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Represents a gallery image model.
+    /// </summary>
+    [JsonProperty("description")]
+    public string Description { get; set; }
+
+    /// <summary>
+    /// Represents the URL property of a GalleryImageModel.
+    /// </summary>
+    [JsonProperty("url")]
+    public string Url { get; set; }
+
+    /// <summary>
+    /// Represents the date and time when the gallery image was created.
+    /// </summary>
+    /// <remarks>
+    /// This property is of type DateTime and is used in the GalleryImageModel struct.
+    /// It indicates the exact point in time when the gallery image was created.
+    /// </remarks>
+    [JsonProperty("created")]
+    public DateTime Created { get; set; }
+}
