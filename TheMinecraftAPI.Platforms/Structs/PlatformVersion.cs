f988d53890098dd85ec517f8649a7c8266b1dba5
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-03-24 01:38:12 -0400

    Add PlatformVersion struct
    This commit introduces a new structure called PlatformVersion in the Platforms namespace. This struct includes multiple properties such as Id, UploadDate, Name, FileName, and FileSize, essential for handling platform version data in the application.

---
+++ TheMinecraftAPI.Platforms/Structs/PlatformVersion.cs
@@ -0,0 +1,12 @@
+﻿namespace TheMinecraftAPI.Platforms.Structs;
+
+public struct PlatformVersion
+{
+    public string Id { get; set; }
+    public DateTime UploadDate { get; set; }
+    public string Name { get; set; }
+    public string FileName { get; set; }
+    public long FileSize { get; set; }
+
+
+}
