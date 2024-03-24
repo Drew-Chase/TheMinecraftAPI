2f0fbab88faa75e9eba60e8dcd682ce79c1d336f
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-03-24 01:38:50 -0400

    Extend search functionality in IPlatformClient
    A new parameter, 'gameVersion', has been added to the SearchProjects method in the IPlatformClient interface to allow for filtering by Minecraft version. Furthermore, a new 'AdvancedSearchProjects' method has been implemented for more flexible project searching. A 'GetAuthorProfileImage' method has also been introduced to retrieve author profile images by username.

--- TheMinecraftAPI.Platforms/Clients/IPlatformClient.cs
+++ TheMinecraftAPI.Platforms/Clients/IPlatformClient.cs
@@ -10,10 +10,21 @@ public interface IPlatformClient
     /// <param name="query">The search query.</param>
     /// <param name="projectType">The project type to filter by.</param>
     /// <param name="loader">The loader to filter by.</param>
+    /// <param name="gameVersion">The minecraft version.</param>
     /// <param name="limit">The maximum number of projects to retrieve.</param>
     /// <param name="offset">The number of projects to skip from the beginning of the results.</param>
     /// <returns>An array of <see cref="PlatformModel"/> representing the search results.</returns>
-    public Task<PlatformSearchResults> SearchProjects(string query, string projectType, string loader, int limit, int offset);
+    public Task<PlatformSearchResults> SearchProjects(string query, string projectType, string loader, string gameVersion, int limit, int offset);
+
+    /// <summary>
+    /// Performs an advanced search for projects based on the specified search criteria.
+    /// </summary>
+    /// <param name="query">The search query string.</param>
+    /// <param name="limit">The maximum number of projects to retrieve.</param>
+    /// <param name="offset">The number of projects to skip before starting to retrieve.</param>
+    /// <param name="options">The advanced search options.</param>
+    /// <returns>The search results containing the matching projects.</returns>
+    public Task<PlatformSearchResults> AdvancedSearchProjects(string query, int limit, int offset, AdvancedSearchOptions options);
 
     /// <summary>
     /// Retrieves a platform project by its ID.
@@ -30,5 +41,12 @@ public interface IPlatformClient
     /// <param name="type">The type of the project (mod, modpack, resourcepack, etc.).</param>
     /// <returns>The URL of the project icon.</returns>
     public Task<string> GetProjectIcon(string id, string type);
+
+    /// <summary>
+    /// Retrieves the URL of the author's profile image.
+    /// </summary>
+    /// <param name="username">The username of the author.</param>
+    /// <returns>The URL of the author's profile image as a string.</returns>
     public Task<string> GetAuthorProfileImage(string username);
+
 }
