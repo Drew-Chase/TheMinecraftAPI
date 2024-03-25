14bbc4540e12ef910daba4f6b2db2455732adfd9
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-03-25 10:30:10 -0400

    Add PlatformVersionDependencyType enum
    A new enumeration file, PlatformVersionDependencyType.cs, has been added in the Platforms folder. This enum includes four types: Required, Optional, Embedded and Unknown, detailing the various dependency types for a platform version.

---
+++ TheMinecraftAPI.Platforms/PlatformVersionDependencyType.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace TheMinecraftAPI.Platforms.Structs;
+
+public enum PlatformVersionDependencyType
+{
+    /// <summary>
+    /// Represents a dependency that is required by the platform version.
+    /// </summary>
+    [JsonPropertyName("required")] [JsonProperty("required")]
+    Required,
+
+    /// <summary>
+    /// Represents a dependency that is optional for the platform version.
+    /// </summary>
+    [JsonPropertyName("optional")] [JsonProperty("optional")]
+    Optional,
+
+    /// <summary>
+    /// Represents a dependency type of an embedded platform version.
+    /// </summary>
+    [JsonPropertyName("embedded")] [JsonProperty("embedded")]
+    Embedded,
+
+    /// <summary>
+    /// Represents an unknown dependency type for a platform version.
+    /// </summary>
+    [JsonPropertyName("unknown")] [JsonProperty("unknown")]
+    Unknown
+}
