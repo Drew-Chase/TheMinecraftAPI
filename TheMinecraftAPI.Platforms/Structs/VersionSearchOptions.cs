ab38374603b76a6c03e9f16e6b0490ec33841f35
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-03-25 13:28:50 -0400

    Add VersionSearchOptions struct
    A new struct, VersionSearchOptions, has been added to the TheMinecraftAPI.Platforms.Structs namespace. This struct includes arrays for GameVersions, Loaders, and ReleaseTypes, providing search options for different versions.

---
+++ TheMinecraftAPI.Platforms/Structs/VersionSearchOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace TheMinecraftAPI.Platforms.Structs;
+
+public struct VersionSearchOptions
+{
+    public string[] GameVersions {get;set;}
+    public string[] Loaders {get;set;}
+    public ReleaseType[] ReleaseTypes {get;set;}
+}
