9ee9da7390947018d2d1f1d87638e158e3882328
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-03-25 19:19:26 -0400

    Add new structures for Minecraft mod loader
    This commit introduces the addition of new structures to the Minecraft mod loader. It includes the `LoaderVersion` and `LoaderVersionFile` structs, as well as an enumeration named `LoaderFileType`. These additions will help to better organize information relating to the different types of Minecraft mod loader files.

---
+++ TheMinecraftAPI.ModLoaders/Structs/LoaderVersion.cs
@@ -0,0 +1,22 @@
+﻿namespace TheMinecraftAPI.ModLoaders.Structs;
+
+public enum LoaderFileType
+{
+    Server,
+    Client,
+    Installer
+}
+
+public struct LoaderVersion
+{
+    public string Version { get; set; }
+    public string MinecraftVersion { get; set; }
+    public LoaderVersionFile[] Files { get; set; }
+}
+
+public struct LoaderVersionFile
+{
+    public string FileName { get; set; }
+    public Uri Url { get; set; }
+    public LoaderFileType Type { get; set; }
+}
