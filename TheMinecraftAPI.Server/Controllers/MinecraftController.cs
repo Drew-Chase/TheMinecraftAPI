baa09c7a99dcba4514e53cd4fefe1f41aafd28aa
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-03-25 11:35:51 -0400

    Add comments to MinecraftController methods
    Updated comments for all defined methods inside the MinecraftController file. The comments provide a brief explanation of what the method does, its parameters, and return values, facilitating understanding and readability of the code.

--- TheMinecraftAPI.Server/Controllers/MinecraftController.cs
+++ TheMinecraftAPI.Server/Controllers/MinecraftController.cs
@@ -9,6 +9,12 @@ namespace TheMinecraftAPI.Server.Controllers;
 [ApiController]
 public class MinecraftController : ControllerBase
 {
+    /// <summary>
+    /// Retrieves the status of a Minecraft server asynchronously.
+    /// </summary>
+    /// <param name="url">The URL of the Minecraft server.</param>
+    /// <param name="port">The port number of the Minecraft server. Default value is 25565.</param>
+    /// <returns>A task representing the asynchronous operation. The task result is an object containing the server status information.</returns>
     [HttpGet("server"), ResponseCache(Duration = 60)] // Cache for 1 minute
     public async Task<IActionResult> GetServerStatusAsync([FromQuery] string url, [FromQuery] int port = 25565)
     {
@@ -34,6 +40,12 @@ public class MinecraftController : ControllerBase
         }
     }
 
+    /// <summary>
+    /// Retrieves the available versions of Minecraft.
+    /// </summary>
+    /// <param name="majorVersion">Optional filter for a specific major version.</param>
+    /// <param name="snapshots">Indicates if only snapshot versions should be included.</param>
+    /// <returns>An object containing the list of available versions.</returns>
     [HttpGet("versions"), ResponseCache(Duration = 3600)] // Cache for 1 hour
     public async Task<IActionResult> GetVersionsAsync([FromQuery(Name = "major_version")] Version? majorVersion = null, [FromQuery] bool snapshots = false)
     {
@@ -41,6 +53,13 @@ public class MinecraftController : ControllerBase
         return Ok(response);
     }
 
+    /// <summary>
+    /// Retrieves the Java Runtime Environment (JRE) binaries for the specified operating system asynchronously.
+    /// </summary>
+    /// <param name="operatingSystem">The operating system to retrieve the JRE binaries for.
+    /// Leave empty or specify "platforms" to retrieve the list of supported platforms.</param>
+    /// <returns>A task representing the asynchronous operation. The task result is an IActionResult
+    /// containing the JRE binaries or a list of supported platforms.</returns>
     [HttpGet("java/{operatingSystem?}"), ResponseCache(Duration = 86400)] // Cache for 24 hours
     public async Task<IActionResult> GetJavaAsync([FromRoute] string operatingSystem = "")
     {
@@ -78,6 +97,11 @@ public class MinecraftController : ControllerBase
         }
     }
 
+    /// <summary>
+    /// Retrieves the assets of a specific Minecraft version.
+    /// </summary>
+    /// <param name="version">The version of Minecraft.</param>
+    /// <returns>A task representing the asynchronous operation. The task result is an array of objects representing the assets.</returns>
     [HttpGet("version/{version}/assets"), ResponseCache(Duration = 86400)] // Cache for 24 hours
     public async Task<IActionResult> GetAssets([FromRoute] string version)
     {
@@ -96,6 +120,11 @@ public class MinecraftController : ControllerBase
         }
     }
 
+    /// <summary>
+    /// Retrieves the available JAR files for a specific version of Minecraft.
+    /// </summary>
+    /// <param name="version">The version of Minecraft.</param>
+    /// <returns>An object containing the list of available JAR files.</returns>
     [HttpGet("version/{version}/jars"), ResponseCache(Duration = 86400)] // Cache for 24 hours
     public async Task<IActionResult> GetJars([FromRoute] string version)
     {
