246f95356b8c5b700072b705ef9731e6be3bed34
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-03-25 10:29:59 -0400

    Add ReleaseType enum in Platforms
    This commit introduces a new enum, ReleaseType, in the Platforms namespace. The enum includes identifiers for different types of releases, such as Release, Beta, Alpha, and Unknown. This will help categorize the different types of platform versions.

---
+++ TheMinecraftAPI.Platforms/ReleaseType.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace TheMinecraftAPI.Platforms.Structs;
+
+/// <summary>
+/// Represents the type of a release.
+/// </summary>
+public enum ReleaseType
+{
+    /// <summary>
+    /// Enum member representing a release type.
+    /// </summary>
+    [JsonPropertyName("release")] [JsonProperty("release")]
+    Release,
+
+    /// <summary>
+    /// Represents the beta release type of a platform version.
+    /// </summary>
+    [JsonPropertyName("beta")] [JsonProperty("beta")]
+    Beta,
+
+    /// <summary>
+    /// Represents the Alpha release type of a platform version.
+    /// </summary>
+    [JsonPropertyName("alpha")] [JsonProperty("alpha")]
+    Alpha,
+
+    /// <summary>
+    /// The Unknown release type.
+    /// </summary>
+    [JsonPropertyName("unknown")] [JsonProperty("unknown")]
+    Unknown
+}
