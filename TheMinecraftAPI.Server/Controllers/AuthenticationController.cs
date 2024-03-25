0985b9697b601753ef6d2e6feb39c5b0f5b93eba
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-03-25 16:28:41 -0400

    Add AuthenticationController with GitHub OAuth
    This commit introduces a new controller, AuthenticationController, for managing authentication. It includes a GitHub OAuth endpoint that takes in parameters like the code, state, and error details and redirects with errors if present.

---
+++ TheMinecraftAPI.Server/Controllers/AuthenticationController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheMinecraftAPI.Server.Controllers;
+
+[Route("auth")]
+[ApiController]
+public class AuthenticationController : ControllerBase
+{
+    //https://api.theminecraftapi.com/auth/oauth/github?error=&error_description=&error_uri=
+    [HttpGet("oauth/github")]
+    public IActionResult OAuth([FromQuery] string code, [FromQuery] string? state, [FromQuery] string? error, [FromQuery(Name = "error_description")] string? errorDescription, [FromQuery(Name = "error_uri")] Uri? errorUri)
+    {
+        if (!string.IsNullOrWhiteSpace(error))
+        {
+            Redirect($"https://theminecraftapi.com/auth/login?error={error}&error_description={errorDescription}&error_uri={errorUri}");
+        }
+        // Add cookie to client with access token
+
+
+
+        return Ok();
+    }
+}
