6a69ef952e559fd9e83850da99b3e412bbfabc03
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-03-25 10:30:40 -0400

    Add PlatformVersionDependencies struct
    A new struct, PlatformVersionDependencies, has been added. This struct will represent the dependencies of a Minecraft platform version. It includes properties to represent the version of a platform, the version ID of a

---
+++ TheMinecraftAPI.Platforms/Structs/PlatformVersionDependencies.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace TheMinecraftAPI.Platforms.Structs;
+
+/// <summary>
+/// Represents the dependencies of a platform version.
+/// </summary>
+public struct PlatformVersionDependencies
+{
+    /// <summary>
+    /// Represents the version of a platform.
+    /// </summary>
+    [JsonPropertyName("id")]
+    [JsonProperty("id")]
+    public required string Id { get; set; }
+
+    /// <summary>
+    /// Represents the version ID of a platform version dependency.
+    /// </summary>
+    [JsonPropertyName("version-id")]
+    [JsonProperty("version-id")]
+    public required string? VersionId { get; set; }
+
+    /// <summary>
+    /// Represents the dependencies of a platform version.
+    /// </summary>
+    [JsonPropertyName("type")]
+    [JsonProperty("type")]
+    public required PlatformVersionDependencyType Type { get; set; }
+}
