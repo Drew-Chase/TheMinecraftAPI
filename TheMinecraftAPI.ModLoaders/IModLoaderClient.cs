714a7be1d4910acdcc33bc7537b07bb20da46d2a
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-03-25 19:19:15 -0400

    Add IModLoaderClient interface
    A new interface, IModLoaderClient, has been added to the ModLoaders namespace. This interface includes the GetVersions() and GetVersion() methods designed for the async retrieval of LoaderVersion instances.

---
+++ TheMinecraftAPI.ModLoaders/IModLoaderClient.cs
@@ -0,0 +1,9 @@
+﻿using TheMinecraftAPI.ModLoaders.Structs;
+
+namespace TheMinecraftAPI.ModLoaders;
+
+public interface IModLoaderClient
+{
+    public Task<LoaderVersion[]> GetVersions();
+    public Task<LoaderVersion[]> GetVersion(string versionId);
+}
