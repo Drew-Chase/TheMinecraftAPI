af0acb9bcd93b889dd9bb6632458e6a3057b8113
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-03-25 16:28:51 -0400

    Add Github OAuth registration in AuthenticationManager
    Introduced a new function 'RegisterGithubOAuth' in the AuthenticationManager class. This function facilitates Github authentication by fetching and validating the access token. The method creates a new user entity with a unique API token and the obtained Github access token.

---
+++ TheMinecraftAPI.Authentication/AuthenticationManager.cs
@@ -0,0 +1,33 @@
+﻿using Chase.CommonLib.Networking;
+using Newtonsoft.Json.Linq;
+using TheMinecraftAPI.Authentication.Entities;
+
+namespace TheMinecraftAPI.Authentication;
+
+public class AuthenticationManager
+{
+    public static async Task<UserEntity> RegisterGithubOAuth(string code)
+    {
+        JObject? json;
+        using (AdvancedNetworkClient client = new())
+        {
+            // Get the access token from Github
+            json = await client.GetAsJson(new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri($"https://github.com/login/oauth/access_token?client_id={Environment.GetEnvironmentVariable("GITHUB_CLIENT_ID")}&client_secret={Environment.GetEnvironmentVariable("GITHUB_CLIENT_SECRET")}&code={code}"),
+                Headers = { { "User-Agent", "TheMinecraftAPI" }, { "Accept", "application/json" } },
+            });
+        }
+
+        // Check if the access token is present
+        if (json?["access_token"] is not { } token) throw new Exception("Failed to get access token from Github");
+
+        // Return the user entity
+        return new UserEntity()
+        {
+            ApiToken = Guid.NewGuid(),
+            AccessToken = token.ToString(),
+        };
+    }
+}
