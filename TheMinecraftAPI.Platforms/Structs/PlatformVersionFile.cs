9f8fb9d15cc0ce73a5750239172c0a5ab8f846f5
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-03-25 10:30:56 -0400

    Add PlatformVersionFile struct in 'Platform' namespace
    This commit introduces the PlatformVersionFile struct to the 'Platform' namespace. This struct is designed to help handle files in a platform version. It includes properties such as URL, filename, file size, primary status and file hash.

---
+++ TheMinecraftAPI.Platforms/Structs/PlatformVersionFile.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace TheMinecraftAPI.Platforms.Structs;
+
+/// <summary>
+/// Represents a file in a platform version.
+/// </summary>
+public struct PlatformVersionFile
+{
+    /// <summary>
+    /// Represents a URL property of a <see cref="PlatformVersionFile"/>.
+    /// </summary>
+    [JsonPropertyName("url")]
+    [JsonProperty("url")]
+    public required Uri? Url { get; set; }
+
+    /// <summary>
+    /// Represents the file name of a platform version.
+    /// </summary>
+    [JsonPropertyName("file-name")]
+    [JsonProperty("file-name")]
+    public required string FileName { get; set; }
+
+    /// <summary>
+    /// Represents the file size of a platform version file.
+    /// </summary>
+    [JsonPropertyName("file-size")]
+    [JsonProperty("file-size")]
+    public required long FileSize { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether this file is the primary file.
+    /// </summary>
+    /// <value>
+    /// <c>true</c> if this file is the primary file; otherwise, <c>false</c>.
+    /// </value>
+    [JsonPropertyName("primary")]
+    [JsonProperty("primary")]
+    public required bool Primary { get; set; }
+
+    /// <summary>
+    /// Represents a file hash.
+    /// </summary>
+    [JsonPropertyName("hash")]
+    [JsonProperty("hash")]
+    public required string? Hash { get; set; }
+}
