43bf364127f55f82a60c78fe747a7c9e6677d90e
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-03-25 12:13:39 -0400

    Add logging to ErrorController
    A new method has been added to the ErrorController in The Minecraft API. This method handles error requests and logs the error using Serilog, detailing the HTTP status code and the URL of the request that caused the error.

--- TheMinecraftAPI.Server/Controllers/ErrorController.cs
+++ TheMinecraftAPI.Server/Controllers/ErrorController.cs
@@ -7,6 +7,12 @@ namespace TheMinecraftAPI.Server.Controllers;
 [ApiController]
 public class ErrorController : ControllerBase
 {
+    /// <summary>
+    /// Handles error requests and logs the error using Serilog.
+    /// </summary>
+    /// <param name="code">The HTTP status code of the error.</param>
+    /// <param name="url">The URL of the request that caused the error.</param>
+    /// <returns>An IActionResult representing the response to the error request.</returns>
     [HttpGet("{code:int}")]
     public IActionResult Index([FromRoute]int code, [FromQuery] string url)
     {
