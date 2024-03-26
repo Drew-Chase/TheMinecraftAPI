57bea03d82c69d0d3ee526a47e58e69ebb37accd
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-03-26 19:25:49 -0400

    Add detailed comments to Files class
    This commit adds thorough inline commenting to the Files class in /TheMinecraftAPI.Server/Data. The comments describe the usage and purpose of the application files, making it easier for developers to understand their function in context.

--- TheMinecraftAPI.Server/Data/Files.cs
+++ TheMinecraftAPI.Server/Data/Files.cs
@@ -1,11 +1,41 @@
 ﻿namespace TheMinecraftAPI.Server.Data;
 
-using static TheMinecraftAPI.Server.Data.Directories;
+using static Directories;
 
+/// <summary>
+/// The <c>Files</c> class provides access to file paths used in the application.
+/// </summary>
 public static class Files
 {
+    /// <summary>
+    /// Represents the configuration of the application.
+    /// </summary>
     public static string ApplicationConfiguration { get; } = Path.Combine(Root, "application.json");
+
+    /// <summary>
+    /// Gets the path of the latest log file.
+    /// </summary>
+    /// <value>The path of the latest log file.</value>
     public static string LatestLog { get; } = Path.Combine(Logs, "latest.log");
+
+    /// <summary>
+    /// Represents the path to the error log file.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// This property provides the path to the error log file. The error log file is used for logging error messages and exceptions.
+    /// </para>
+    /// <para>
+    /// The error log file is named "error.log" and is located in the "logs" directory under the root directory of the application.
+    /// </para>
+    /// </remarks>
     public static string ErrorLog { get; } = Path.Combine(Logs, "error.log");
+
+    /// <summary>
+    /// Gets the path of the debug log file.
+    /// </summary>
+    /// <value>
+    /// The path of the debug log file.
+    /// </value>
     public static string DebugLog { get; } = Path.Combine(Logs, "debug.log");
 }
