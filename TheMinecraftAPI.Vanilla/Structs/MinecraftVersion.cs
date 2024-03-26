a7cc9a40d5332dfb885f2e7024d61c4aafd29154
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-03-26 11:42:04 -0400

    Add MinecraftVersion structs
    Introduced structs for tracking Minecraft version history in TheMinecraftAPI.Vanilla. These structs, MinecraftVersion and MinecraftVersionHistory, handle information about Minecraft releases and snapshots including their ID, type, time, release time and whether they are the latest version.

---
+++ TheMinecraftAPI.Vanilla/Structs/MinecraftVersion.cs
@@ -0,0 +1,16 @@
+﻿namespace TheMinecraftAPI.Vanilla.Structs;
+
+public struct MinecraftVersionHistory
+{
+    public MinecraftVersion[] Releases { get; set; }
+    public MinecraftVersion[] Snapshots { get; set; }
+}
+
+public struct MinecraftVersion
+{
+    public string Id {get;set;}
+    public string Type {get;set;}
+    public DateTime Time {get;set;}
+    public DateTime ReleaseTime {get;set;}
+    public bool Latest { get; set; }
+}
