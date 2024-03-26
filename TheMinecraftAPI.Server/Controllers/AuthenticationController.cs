dc9ee644924848c3feb79aaf68cfcf00918fd113
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-03-26 19:26:29 -0400

    Add documentation to AuthenticationController
    Enhance the AuthenticationController in TheMinecraftAPI.Server by adding summary documentation for the controller itself and for the OAuth method. Increased clarity will help developers to understand the code's purpose and function more effectively.

--- TheMinecraftAPI.Server/Controllers/AuthenticationController.cs
+++ TheMinecraftAPI.Server/Controllers/AuthenticationController.cs
@@ -2,11 +2,23 @@
 
 namespace TheMinecraftAPI.Server.Controllers;
 
+/// <summary>
+/// The AuthenticationController handles user authentication.
+/// </summary>
 [Route("auth")]
 [ApiController]
+[ApiExplorerSettings(IgnoreApi = true)]
 public class AuthenticationController : ControllerBase
 {
-    //https://api.theminecraftapi.com/auth/oauth/github?error=&error_description=&error_uri=
+    /// <summary>
+    /// Handle OAuth authentication with GitHub.
+    /// </summary>
+    /// <param name="code">The authorization code returned by GitHub after user authorization.</param>
+    /// <param name="state">The optional state parameter.</param>
+    /// <param name="error">The optional error message.</param>
+    /// <param name="errorDescription">The optional error description.</param>
+    /// <param name="errorUri">The optional error URI.</param>
+    /// <returns>An <see cref="IActionResult"/> representing the result of the authentication process.</returns>
     [HttpGet("oauth/github")]
     public IActionResult OAuth([FromQuery] string code, [FromQuery] string? state, [FromQuery] string? error, [FromQuery(Name = "error_description")] string? errorDescription, [FromQuery(Name = "error_uri")] Uri? errorUri)
     {
@@ -15,7 +27,6 @@ public class AuthenticationController : ControllerBase
             Redirect($"https://theminecraftapi.com/auth/login?error={error}&error_description={errorDescription}&error_uri={errorUri}");
         }
         // Add cookie to client with access token
-
 
 
         return Ok();
