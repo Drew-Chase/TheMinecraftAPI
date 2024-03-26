1eed87c2591c603b5776053e5f4ede0d139ce65c
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-03-26 11:43:06 -0400

    Update LoaderVersion struct to include JSON attributes
    This update enhances struct LoaderVersion to work with JSON serialization and deserialization. The attributes added enable the object properties to be mapped to and from JSON keys seamlessly. Several summaries have also been included to provide more detailed context about the struct and its properties.

--- TheMinecraftAPI.ModLoaders/Structs/LoaderVersion.cs
+++ TheMinecraftAPI.ModLoaders/Structs/LoaderVersion.cs
@@ -1,22 +1,54 @@
-﻿namespace TheMinecraftAPI.ModLoaders.Structs;
+﻿using System.Text.Json.Serialization;
+using Newtonsoft.Json;
 
-public enum LoaderFileType
-{
-    Server,
-    Client,
-    Installer
-}
+namespace TheMinecraftAPI.ModLoaders.Structs;
 
+/// <summary>
+/// Represents a loader version.
+/// </summary>
 public struct LoaderVersion
 {
+    /// <summary>
+    /// Represents a version of a mod loader.
+    /// </summary>
+    [JsonProperty("version")]
+    [JsonPropertyName("version")]
     public string Version { get; set; }
-    public string MinecraftVersion { get; set; }
-    public LoaderVersionFile[] Files { get; set; }
+
+    /// <summary>
+    /// Represents a version of Minecraft supported by a mod loader.
+    /// </summary>
+    [JsonProperty("minecraft-version")]
+    [JsonPropertyName("minecraft-version")]
+    public string? MinecraftVersion { get; set; }
+
+    /// <summary>
+    /// Represents a loader version file.
+    /// </summary>
+    [JsonProperty("files")]
+    [JsonPropertyName("files")]
+    public LoaderVersionFile[]? Files { get; set; }
 }
 
+/// <summary>
+/// Represents a file associated with a loader version.
+/// </summary>
 public struct LoaderVersionFile
 {
+    /// <summary>
+    /// Represents a file in a loader version.
+    /// </summary>
+    [JsonProperty("file-name")]
+    [JsonPropertyName("file-name")]
     public string FileName { get; set; }
+
+    /// <summary>
+    /// Represents the URL property of a LoaderVersionFile.
+    /// </summary>
+    /// <value>
+    /// The URL of the loader version file.
+    /// </value>
+    [JsonProperty("url")]
+    [JsonPropertyName("url")]
     public Uri Url { get; set; }
-    public LoaderFileType Type { get; set; }
 }
