8f8caf14fab1555d981bca471356b830fa2a2160
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-03-26 19:25:38 -0400

    Add detailed comments to Directories class
    The commit introduces detailed summary comments for the Directories class and its properties – Root and Logs. These comments provide comprehensive understanding of the purpose of the class and the significance of its properties within the application, fostering easier navigation and modification in future.

--- TheMinecraftAPI.Server/Data/Directories.cs
+++ TheMinecraftAPI.Server/Data/Directories.cs
@@ -1,7 +1,17 @@
 ﻿namespace TheMinecraftAPI.Server.Data;
 
+/// <summary>
+/// Provides static properties representing various directories used in the application.
+/// </summary>
 public static class Directories
 {
+    /// <summary>
+    /// Represents the root directory used in the application.
+    /// </summary>
     public static string Root { get; } = Directory.CreateDirectory(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data")).FullName;
+
+    /// <summary>
+    /// Provides static properties representing various directories used in the application.
+    /// </summary>
     public static string Logs { get; } = Directory.CreateDirectory(Path.Combine(Root, "logs")).FullName;
 }
