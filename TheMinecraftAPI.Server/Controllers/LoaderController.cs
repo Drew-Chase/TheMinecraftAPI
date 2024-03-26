254281d91ca3f15b048506fd868f751ea9ec40d8
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-03-26 11:42:43 -0400

    Add LoaderController to handle mod loaders
    The new LoaderController of the Server project makes requests to different Minecraft mod loaders. It mainly includes methods to get loader types, versions, and installers. Furthermore, it provides a method that fetches a specific version's installer. This addition contributes to extending the functionality of the Minecraft API by supporting more mod capabilities.

---
+++ TheMinecraftAPI.Server/Controllers/LoaderController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheMinecraftAPI.ModLoaders.Clients;
+using TheMinecraftAPI.ModLoaders.Structs;
+
+namespace TheMinecraftAPI.Server.Controllers;
+
+[ApiController]
+[Route("loader")]
+public class LoaderController : ControllerBase
+{
+    private static readonly string[] LoaderTypes = new[]
+    {
+        "fabric",
+        "forge"
+    };
+
+    [HttpGet]
+    [ProducesResponseType(typeof(string[]), 200)]
+    public IActionResult GetLoaderTypes()
+    {
+        return Ok(LoaderTypes);
+    }
+
+    [HttpGet("{type}/loaders")]
+    [ProducesResponseType(typeof(LoaderVersion[]), 200)]
+    public async Task<IActionResult> GetLoaderVersions(string type, [FromQuery] string? gameVersion = null)
+    {
+        IModLoaderClient? client = type switch
+        {
+            "fabric" => new FabricClient(),
+            "forge" => new ForgeClient(),
+            _ => null
+        };
+        if (client is null) return NotFound(LoaderTypes);
+        var versions = await client.GetVersions(gameVersion);
+        client.Dispose();
+        return Ok(versions);
+    }
+
+    [HttpGet("{type}/installers")]
+    [ProducesResponseType(typeof(LoaderVersion[]), 200)]
+    public async Task<IActionResult> GetLoaderInstallers(string type, [FromQuery] string? gameVersion = null)
+    {
+        IModLoaderClient? client = type switch
+        {
+            "fabric" => new FabricClient(),
+            "forge" => new ForgeClient(),
+            _ => null
+        };
+        if (client is null) return NotFound(LoaderTypes);
+        var versions = await client.GetInstallers(gameVersion);
+        client.Dispose();
+        return Ok(versions);
+    }
+
+
+    [HttpGet("{type}/installer/{version}")]
+    [ProducesResponseType(typeof(LoaderVersion[]), 200)]
+    public async Task<IActionResult> GetLoaderVersion([FromRoute] string type, [FromRoute] string version, [FromQuery] string? gameVersion = null)
+    {
+        IModLoaderClient? client = type switch
+        {
+            "fabric" => new FabricClient(),
+            "forge" => new ForgeClient(),
+            _ => null
+        };
+        if (client is null) return NotFound(LoaderTypes);
+        var versions = await client.GetInstaller(version, gameVersion);
+        client.Dispose();
+        return Ok(versions);
+    }
+}
