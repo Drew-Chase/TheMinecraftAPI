8d7396b762a533deb1b556c45025dd0c7e6de1b0
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-03-26 19:25:05 -0400

    Add detailed comments to ApplicationConfiguration class
    The comments added provide a clearer understanding of the class ApplicationConfiguration and its properties. Each property is now explained with a summary and additional remarks where necessary. This improves code readability and maintainability.

--- TheMinecraftAPI.Server/Data/ApplicationConfiguration.cs
+++ TheMinecraftAPI.Server/Data/ApplicationConfiguration.cs
@@ -4,13 +4,31 @@ using Serilog.Events;
 
 namespace TheMinecraftAPI.Server.Data;
 
+/// <summary>
+/// Represents the configuration settings for the application.
+/// </summary>
 public class ApplicationConfiguration : AppConfigBase<ApplicationConfiguration>
 {
+    /// <summary>
+    /// Represents the port used by the application.
+    /// </summary>
+    /// <remarks>
+    /// The port indicates the network port number on which the application listens for incoming requests.
+    /// </remarks>
     [JsonProperty("port")] public int Port { get; set; } = 8080;
 
+    /// <summary>
+    /// Represents the encryption salt used for encrypting data.
+    /// </summary>
     [JsonProperty("encryption-key")] public string EncryptionSalt { get; set; } = Guid.NewGuid().ToString("N");
 
+    /// <summary>
+    /// Represents the log level for logging events.
+    /// </summary>
     [JsonProperty("log-level")] public LogEventLevel LogLevel { get; set; } = LogEventLevel.Information;
 
+    /// <summary>
+    /// Represents the startup time of the application.
+    /// </summary>
     [JsonIgnore] public DateTime StartupTime { get; } = DateTime.Now;
 }
